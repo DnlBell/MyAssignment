33312a991088f983d0d3d6fab6199039cb471ee5
dnlbell <dnl.bell@gmail.com>
2018-04-19 11:04:08 -0700

    test building

--- app/src/androidTest/java/dnlbell/org/myassignment/MainActivityTest.java
+++ app/src/androidTest/java/dnlbell/org/myassignment/MainActivityTest.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
