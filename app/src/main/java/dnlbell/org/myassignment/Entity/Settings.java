9ac07bf5f58fd40b207d1b4530df847e15463fba
dnlbell <dnl.bell@gmail.com>
2018-05-21 22:23:28 -0700

    Room db entity Settings and Dao

--- app/src/main/java/dnlbell/org/myassignment/Entity/Settings.java
+++ app/src/main/java/dnlbell/org/myassignment/Entity/Settings.java
@@ -1,4 +1,122 @@
 package dnlbell.org.myassignment.Entity;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+
+@Entity
 public class Settings {
+    @PrimaryKey
+    @NonNull
+    private int id = 0;
+
+    @ColumnInfo(name = "hour")
+    private int hour;
+
+    @ColumnInfo(name = "minute")
+    private int minute;
+
+    @ColumnInfo(name = "meridiem")
+    private boolean meridiem;
+
+    @ColumnInfo(name = "radius")
+    private int radius;
+
+    @ColumnInfo(name = "sexuality")
+    private String sexuality;
+
+    @ColumnInfo(name = "gender")
+    private String gender;
+
+    @ColumnInfo(name = "rangeLow")
+    private int rangeLow;
+
+    @ColumnInfo(name = "rangeHigh")
+    private int rangeHigh;
+
+    @ColumnInfo(name = "privacy")
+    private boolean privacy;
+
+    @NonNull
+    public int getId() {
+        return id;
+    }
+
+    public void setId(@NonNull int id) {
+        this.id = id;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public boolean isMeridiem() {
+        return meridiem;
+    }
+
+    public void setMeridiem(boolean meridiem) {
+        this.meridiem = meridiem;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public String getSexuality() {
+        return sexuality;
+    }
+
+    public void setSexuality(String sexuality) {
+        this.sexuality = sexuality;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getRangeLow() {
+        return rangeLow;
+    }
+
+    public void setRangeLow(int rangeLow) {
+        this.rangeLow = rangeLow;
+    }
+
+    public int getRangeHigh() {
+        return rangeHigh;
+    }
+
+    public void setRangeHigh(int rangeHigh) {
+        this.rangeHigh = rangeHigh;
+    }
+
+    public boolean isPrivacy() {
+        return privacy;
+    }
+
+    public void setPrivacy(boolean privacy) {
+        this.privacy = privacy;
+    }
 }
---
+++ app/src/main/java/dnlbell/org/myassignment/settingsDao.java
@@ -0,0 +1,29 @@
+package dnlbell.org.myassignment;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import dnlbell.org.myassignment.Entity.Settings;
+
+import java.util.List;
+
+@Dao
+public interface settingsDao {
+    @Query( "SELECT * FROM Settings WHERE id = :mId")
+    List<Settings> getSettingsById(int mId);
+
+    @Update
+    void updateSettings(Settings... settings);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertAll(Settings... settings);
+
+    @Delete
+    void delete(Settings... settings);
+
+}
+
